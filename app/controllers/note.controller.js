ea021787f0a282c6ea5541f091b5094bac8f7183
Hendi Santika <hendisantika@gmail.com>
2018-11-29 06:18:41 +0700

    #12 - Find a single note with a noteId

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -41,6 +41,24 @@ exports.findAll = (req, res) => {
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId)
+        .then(note => {
+            if (!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send(note);
+        }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        });
+    });
 
 };
 
