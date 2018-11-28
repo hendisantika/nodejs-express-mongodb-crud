dad7d22c9e91a596ef76be1c8e8dce082d0f68cd
Hendi Santika <hendisantika@gmail.com>
2018-11-29 06:19:00 +0700

    #13 - Update a note identified by the noteId in the request

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -64,6 +64,35 @@ exports.findOne = (req, res) => {
 
 // Update a note identified by the noteId in the request
 exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    Note.findByIdAndUpdate(req.params.noteId, {
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    }, {new: true})
+        .then(note => {
+            if (!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send(note);
+        }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        return res.status(500).send({
+            message: "Error updating note with id " + req.params.noteId
+        });
+    });
 
 };
 
