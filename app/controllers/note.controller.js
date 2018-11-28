95138ab2e11919cb74d7edeb027fdb3c3e40e0d1
Hendi Santika <hendisantika@gmail.com>
2018-11-29 06:19:20 +0700

    #14 - Delete a note with the specified noteId in the request

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -98,5 +98,23 @@ exports.update = (req, res) => {
 
 // Delete a note with the specified noteId in the request
 exports.delete = (req, res) => {
+    Note.findByIdAndRemove(req.params.noteId)
+        .then(note => {
+            if (!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send({message: "Note deleted successfully!"});
+        }).catch(err => {
+        if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        return res.status(500).send({
+            message: "Could not delete note with id " + req.params.noteId
+        });
+    });
 
 };
